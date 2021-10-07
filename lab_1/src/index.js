79b6ecf5ccdd6b9388b3ada239035ac8b78a3f0b
Alexey Bilko <bilkoalexey67@gmail.com>
2021-10-07 12:35:06 +0300

    added first page

--- lab_1/src/index.js
+++ lab_1/src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Page2 from './page2';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <Switch>
+            <Route exact path="/" ><App /></Route>
+            <Route path="/page2"><Page2 /></Route>
+        </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
