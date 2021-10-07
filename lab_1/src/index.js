f9f2d580d339e09d80887724ffb0f65144068284
AlexeyBilko <53230108+AlexeyBilko@users.noreply.github.com>
2021-10-07 12:44:27 +0300

    Added pages, css (#3)
    * added first page
    
    * added page 2,3,4,5; added css

--- lab_1/src/index.js
+++ lab_1/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Page2 from './page2';
+import Page3 from './page3';
+import Page4 from './page4';
+import Page5 from './page5';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +14,9 @@ ReactDOM.render(
         <Switch>
             <Route exact path="/" ><App /></Route>
             <Route path="/page2"><Page2 /></Route>
+            <Route path="/page3"><Page3 /></Route>
+            <Route path="/page4"><Page4 /></Route>
+            <Route path="/page5"><Page5 /></Route>
         </Switch>
     </Router>
   </React.StrictMode>,
